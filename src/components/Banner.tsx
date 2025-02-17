0c66b70e0ad118a317d55fd9bce2cf7cc5e3e40c
Adrian <adrian.rosipal98@gmail.com>
2025-02-17 01:27:43 +0100

    ome fix

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom"
-import logo from "../assets/logo.png"
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
 export default function Banner() {
   return (
-    <div className="bg-secondary-bg flex flex justify-center items-center h-screen min-w-[120px] ">
+    <div className="h-[80px]  sm:bg-secondary-bg flex flex justify-center items-center h-screen min-w-[120px] ">
       <Link to="/">
         <img src={logo} alt="Banner logo" />
       </Link>
     </div>
-  )
+  );
 }
